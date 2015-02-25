c46cf827060054dcf3b22dcf7c51b25f058fb1dd
Jacob Wiltse <jwiltse@control-tec.com>
2015-02-25 10:28:14 -0500

    Added skeleton files

---
+++ settings.php
@@ -0,0 +1,13 @@
+<?php
+$sitetitle = "My Awesome Media Site";
+
+// Database settings
+$username = "root";
+$password = "mypass";
+$dbname = "awesomesite";
+$host = "localhost";
+
+// Directory settings
+$moviesdir = "/srv/Movies";
+$tvdir = "/srv/TV";
+$musicdir = "/srv/Music";
